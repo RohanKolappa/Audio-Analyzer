a50e59876d9c9c7d819fa2692f1ceaf1ae29fde7
“Rohan <rkolappa@ucdavis.edu>
2021-07-28 16:20:17 -0700

    Added index.html and updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/', methods=["GET"]) #the '/' tells us that we are routing to the home page
+
+def index():
     return "Flask UI test"
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, threaded = True)
+    #debug will allow us to save the file, and the Flask instance will automatically refresh with the latest updates
+    #threaded will allow us to process multiple requests at the same time
 
