4decd63bb8e880a4c1aa764a6699d3f839d02d88
“Rohan <rkolappa@ucdavis.edu>
2021-07-28 17:14:26 -0700

    Test to see if form data is received (using post request)

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"]) #the '/' tells us that we are routing to the home page
+@app.route('/', methods=["GET", "POST"]) #the '/' tells us that we are routing to the home page
+#GET requests are for receiving data
+#POST requests are for taking data from a user input and sending it to another destination
+
 
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True, threaded = True)
-    #debug will allow us to save the file, and the Flask instance will automatically refresh with the latest updates
-    #threaded will allow us to process multiple requests at the same time
+    """debug will allow us to automatically save the file, and the Flask instance will 
+    have the latest updates. In other words, any changes I make to the file will automatically 
+    be sent to the Flask instance once I refresh"""
+    """threaded will allow us to process multiple requests at the same time"""
 
